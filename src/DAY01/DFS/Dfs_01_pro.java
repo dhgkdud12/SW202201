49ef1dfc537434d1d23636eb14e4fa90bdc07693
hy <75581904+dhgkdud12@users.noreply.github.com>
2022-07-19 02:49:55 +0900

    DAY 1 - DFS, BFS

---
+++ src/DAY01/DFS/Dfs_01_pro.java
@@ -0,0 +1,99 @@
+package DAY01.DFS;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Dfs_01_pro {
+    static int N, K;
+    static String[] words;
+    static boolean[] visited;
+    static int selectedCount = 0;
+    static int max = 0;
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.out.println(fib(4));
+
+        System.setIn(new FileInputStream("src/DAY01/Dfs/input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        words = new String[N];
+        visited = new boolean[26];
+
+        visited['a' - 'a'] = true;
+        visited['n' - 'n'] = true;
+        visited['t' - 't'] = true;
+        visited['i' - 'i'] = true;
+        visited['c' - 'c'] = true;
+
+        for (int i = 0; i < N; i++) {
+            words[i] = sc.next().replaceAll("[antic]", ""); // 정규식
+        }
+
+        selectedCount = 5;
+
+        if (K<5) {
+            System.out.println(0);
+        } else if (K==26) {
+            System.out.println(N);
+        } else {
+            max = countWords();
+
+            for (int i = 0; i < 26; i++) {
+                if (visited[i] == false) {
+                    dfs(i);
+                }
+            }
+            System.out.println(max);
+        }
+    }
+    
+    static void dfs(int index) {
+        // 1. 체크인
+        visited[index] = true;
+        selectedCount++;
+        // 2. 목적지인가? : seletedCount == K => 읽을 수 있는 단어 계산
+        if (selectedCount == K) {
+            max = Math.max(max, countWords());
+        } else {
+            // 3. 연결된 곳을 순회 : index + 1 ~ 25
+            for (int i = index+1; i <= 25 ; i++) {
+                // 4. 갈 수 있는가? : 방문 여부
+                if (visited[i] == false)
+                    // 5. 간다 : dfs
+                    dfs(i);
+            }
+        }
+
+        // 6. 체크아웃
+        visited[index] = false;
+        selectedCount--;
+
+    }
+
+    static int countWords() {
+        int count = 0;
+        for (int n = 0; n < N; n++) {
+           boolean isPossible = true;
+           String word = words[n];
+            for (int i = 0; i < word.length(); i++) {
+                if (!visited[word.charAt(i) - 'a']) {
+                    isPossible = false;
+                    break;
+                }
+            }
+            if (isPossible) count++;
+        }
+        return count;
+    }
+
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
---
+++ src/DAY01/DFS/Dfs_02_pro.java
@@ -0,0 +1,70 @@
+package DAY01.DFS;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Dfs_02_pro {
+    static int L, C;
+    static char[] data;
+    static List<String> result;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/DAY01/Dfs/input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        L = sc.nextInt();
+        C = sc.nextInt();
+
+        System.out.println(L + ", " + C);
+
+        data = new char[C];
+        result = new LinkedList<>();
+
+        for (int i = 0; i < C; i++) {
+            data[i] = sc.next().charAt(0);
+        }
+
+        Arrays.sort(data);
+
+        dfs(0, 0, 0, -1, "");
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+
+    }
+    static void dfs(int length, int ja, int mo, int current, String pwd) {
+        //1. 체크인 - 생략 가능
+        //2. 목적지인가? : length == L => ja 개수, mo 개수 확인 암호 가능 판별
+        if (length == L) {
+            if (ja >= 2 && mo >= 1) {
+                result.add(pwd);
+            }
+        } else {
+            //3. 연결된 곳을 순회 : current + 1 ~ C
+            for (int i = current + 1; i < C; i++) {
+                //4. 갈 수 있는가? : 다 갈 수 있음
+                //5. 간다 -> ja, mo
+                if (data[i] == 'a' || data[i] == 'e' || data[i] == 'i' || data[i] == 'o' || data[i] == 'u')
+                    dfs(length+1, ja, mo+1, i, pwd + data[i]);
+                else
+                    dfs(length+1, ja+1, mo, i, pwd + data[i]);
+            }
+        }
+        //6. 체크아웃 - 생략 가능
+
+    }
+}
+
+//1. 체크인
+//2. 목적지인가?
+//3. 연결된 곳을 순회
+    //4. 갈 수 있는가?
+        //5. 간다
+//6. 체크아웃
+
+
